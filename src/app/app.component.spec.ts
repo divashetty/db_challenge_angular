e240b24d0fcf510bf07e656412e2080aaf6d90a0
Divakar Shetty <divakarshetty@192.168.1.112>
2021-02-22 13:00:50 +0000

    reactive form with dynamic calculator  input

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        ReactiveFormsModule,
+        FormsModule
+    ]
     }).compileComponents();
   });
-
+ 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -28,4 +32,36 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('db-challenge-app app is running!');
   });
+
+  it('should display result', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    compiled.querySelector('#firstValue').value = 120;
+    compiled.querySelector('#secondValue').value = 12;
+    expect(compiled.querySelector('#result').value).toEqual(120/12);
+  });
+
+  it('should first divident field valid', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    let divident = compiled.form.controls['firstInputDivisibleValue'];
+    expect(divident.valid).toBeFalsy();
+    divident.setValue("");
+    expect(divident.hasError('required')).toBeTruthy();
+    divident.setValue("123");
+    expect(divident.hasError('minLength')).toBeTruthy();
+});
+it('should second divident field valid', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  fixture.detectChanges();
+  const compiled = fixture.nativeElement;
+  let secDivident = compiled.form.controls['secondInputDivisibleValue'];
+  expect(secDivident.valid).toBeFalsy();
+  secDivident.setValue("");
+  expect(secDivident.hasError('required')).toBeTruthy();
+  secDivident.setValue("3");
+  expect(secDivident.hasError('minLength')).toBeTruthy();
+});
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'db-challenge-app';
+  public title="b-challenge-app";
+  public form!: FormGroup;
+  public purchaseprice!: number;
+  public purchasepricetaxes!: number;
+  public timer:any;
+  constructor(private fb: FormBuilder) {
+    this.createForm();
+    this.form.valueChanges.subscribe(data => {
+      const {firstInputDivisibleValue, secondInputDivisibleValue} =data;
+      
+      clearTimeout(this.timer)
+      if(this.form.invalid) return;
+      this.timer = setTimeout(()=>{
+        this.form.patchValue({
+          divisibleResultValue:  firstInputDivisibleValue / secondInputDivisibleValue
+        })
+      },1000);
+
+    });
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  createForm() {
+    this.form = this.fb.group({
+      firstInputDivisibleValue: ["", Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
+      secondInputDivisibleValue: ["", Validators.pattern(/^-?(0|[1-9]\d*)?$/)],
+      divisibleResultValue: [""]
+    });
+  }
+
+
+  submit() {
+    console.log(this.form);
+  }
 }
